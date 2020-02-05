1e77b0d724cb7f1678eaebacfbce1cce43a354d5
wfrankhauser <william.frankhauser@gmail.com>
2020-02-05 08:22:37 +0000

    minor fixes

--- ec2.tf
+++ ec2.tf
@@ -8,12 +8,19 @@ resource "aws_key_pair" "deployer" {
   public_key = tls_private_key.ec2_instance.public_key_openssh
 }
 
-resource "aws_instance" "web" {
+resource "local_file" "ssh_key" {
+    content     = tls_private_key.ec2_instance.private_key_pem
+    filename = "${path.module}/wfrankhauser_id_rsa"
+}
+
+resource "aws_instance" "api_nodes" {
   count = var.instance_count
   ami           = data.aws_ami.ubuntu.id
-  instance_type = "t2.micro"
-  vpc_security_group_ids = ["${aws_security_group.lb_to_instances.id}"]
+  instance_type = "t2.nano"
+  vpc_security_group_ids = ["${aws_security_group.ssh_to_ec2.id}"]
   subnet_id = aws_subnet.main.id
+  user_data = "${path.module}/sources/user_data.sh"
+  associate_public_ip_address = true
 
   root_block_device {
     encrypted = true
--- output.tf
+++ output.tf
@@ -1,7 +1,11 @@
-output "public_key" {
-  value = tls_private_key.ec2_instance.public_key_openssh
-}
+#output "public_key" {
+#  value = tls_private_key.ec2_instance.public_key_openssh
+#}
 
-output "private_key" {
-  value = tls_private_key.ec2_instance.private_key_pem
+#output "private_key" {
+#  value = tls_private_key.ec2_instance.private_key_pem
+#}
+
+output "host_dns" {
+  value = aws_instance.api_nodes.*.public_dns
 }
